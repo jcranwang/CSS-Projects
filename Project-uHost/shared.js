cd738c1342b99fea47aebae311bbfcf45ffd47b3
jcranwang <jason.cr.wang@gmail.com>
2019-07-03 01:01:20 +0930

    Project-uHost: add side navigation bar

--- Project-uHost/shared.js
+++ Project-uHost/shared.js
@@ -2,10 +2,15 @@ const planBtns = document.querySelectorAll(".plan button");
 const modal = document.querySelector(".modal");
 const backdrop = document.querySelector(".backdrop");
 const modalNo = document.querySelector(".modal-buttons_no");
+const toggleBtn = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 const closeDisplay = () => {
-  modal.style.display = "none";
+  if (modal) {
+    modal.style.display = "none";
+  } 
   backdrop.style.display = "none";
+  mobileNav.style.display = "none";
 };
 
 planBtns.forEach(btn => {
@@ -15,7 +20,12 @@ planBtns.forEach(btn => {
   });
 });
 
-modalNo.addEventListener("click", closeDisplay);
+if (modalNo) {
+  modalNo.addEventListener("click", closeDisplay);
+}
 backdrop.addEventListener("click", closeDisplay);
 
-
+toggleBtn.addEventListener("click", () => {
+  backdrop.style.display = "block";
+  mobileNav.style.display = "block";
+});
